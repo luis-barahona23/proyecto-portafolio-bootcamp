2e96641828b1545043be8aeadefa233a4f52f715
luis.barahona <luis.barahonas@hotmail.com>
2026-03-21 13:50:37 -0300

    Finalizar integracion API pagina Inicio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
-import {santiago, londres, nyc, losAngeles, hk, seul, shangai, paris, tokio, singapur} from './objetos.js';
+import {arregloCiudades1, arregloCiudades2, iconos} from './objetos.js';
+const tarjetas1 = document.querySelectorAll(".tarjeta-1");
+const tarjetas2 = document.querySelectorAll(".tarjeta-2");
+
 function retrasar(ms){
     return new Promise(resolver => setTimeout(resolver, ms));       
 }
@@ -9,24 +12,52 @@ function separarNombre(nombre) {
     
 
 }
+function cicloDia() {
+    const horas = new Date().getHours();
+    if (horas >= 6 && horas <= 18){
+        return "dia"
+    }
+    return "noche"
+}
 async function main() {
-    await Promise.all([
-        santiago.fetchCall(),
-        nyc.fetchCall(),
-        losAngeles.fetchCall(),
-        londres.fetchCall(),
-        hk.fetchCall(),
-    ])
-    
+    await Promise.all(arregloCiudades1.map(ciudad => ciudad.fetchCall()));
     await retrasar(1000);
-    await Promise.all([
-        seul.fetchCall(),
-        shangai.fetchCall(),
-        paris.fetchCall(),
-        tokio.fetchCall(),
-        singapur.fetchCall()])
-
-    console.log(santiago.resultado)
+    await Promise.all(arregloCiudades2.map(ciudad => ciudad.fetchCall()));
+    arregloCiudades1.forEach((ciudad) => console.log(separarNombre(ciudad.resultado)));
+    arregloCiudades2.forEach((ciudad) => console.log(separarNombre(ciudad.resultado)));
+    console.log(arregloCiudades1[0].resultado)
+    tarjetas1.forEach((elemento, index) => {
+        elemento.style.width = "250px"
+        const imagen = document.createElement("img");
+        const nombre = document.createElement("div");
+        const resumen = document.createElement("div");
+        const breakPoint = document.createElement("hr");
+        nombre.textContent = separarNombre(arregloCiudades1[index].resultado);
+        resumen.textContent = `Temperatura Actual: ${arregloCiudades1[index].resultado.current.temperature_2m}°C`;
+        imagen.setAttribute("src", iconos[arregloCiudades1[index].resultado.current.weather_code][cicloDia()]);
+        resumen.classList.add('pb-2');
+        elemento.appendChild(imagen);
+        elemento.appendChild(breakPoint)
+        elemento.appendChild(nombre)
+        elemento.appendChild(breakPoint);
+        elemento.appendChild(resumen);
+    })
+    tarjetas2.forEach((elemento, index) => {
+        elemento.style.width = "300px"
+        const imagen = document.createElement("img");
+        const nombre = document.createElement("div");
+        const resumen = document.createElement("div");
+        const breakPoint = document.createElement("hr");
+        nombre.textContent = separarNombre(arregloCiudades2[index].resultado);
+        resumen.textContent = `Temperatura Actual: ${arregloCiudades2[index].resultado.current.temperature_2m}°C`;
+        imagen.setAttribute("src", iconos[arregloCiudades2[index].resultado.current.weather_code][cicloDia()]);
+        resumen.classList.add('pb-2');
+        elemento.appendChild(imagen);
+        elemento.appendChild(breakPoint);
+        elemento.appendChild(nombre)
+        elemento.appendChild(breakPoint);
+        elemento.appendChild(resumen);
+    })
 }
 
 main()
